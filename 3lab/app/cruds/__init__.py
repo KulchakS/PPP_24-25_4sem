308cae1ad8ce6d55e7678bdc663c4bf89263dfd7
Александр <kulchak2005@mail.ru>
2025-06-05 17:09:48 +0300

    Delete

--- 3lab/app/cruds/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file makes 'cruds' a Python package
-# You would place your CRUD (Create, Read, Update, Delete) operations here,
-# typically for interacting with your database models from Lab 1/2. 
--- 3lab/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file makes 'models' a Python package
-# You would place your SQLAlchemy models or other data models here.
-# For Lab 3, this might include models related to users if authentication is integrated deeply,
-# or models for storing task results persistently if Celery's default backend is not sufficient. 
